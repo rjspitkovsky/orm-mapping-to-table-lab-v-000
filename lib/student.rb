bb38ab23e5e6313de4d39126b12afd411b9b5d5e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-12 19:02:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,9 +40,9 @@ class Student
   end
 
   def self.create(name:, grade:)
-    song = Student.new(name, grade)
-    song.save
-    song
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
 
